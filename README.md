cc54750a18f9e7602750b780b89ab28b5d676e15
Charles E. Lehner <charles.lehner@spruceid.com>
2021-03-01 22:02:20 -0500

    Initial implementation

---
+++ README.md
@@ -0,0 +1,18 @@
+# Degen Verifier
+
+## Get started
+
+Install the dependencies...
+
+```bash
+git clone https://github.com/spruceid/ssi ../ssi
+git clone https://github.com/spruceid/didkit ../didkit
+cd ../didkit/lib/wasm
+cargo install wasm-pack
+wasm-pack build --target web
+cd ../../../degen-verifier
+npm install
+npm run dev
+```
+
+Navigate to [localhost:5000](http://localhost:5000).
---
+++ src/components/Button.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let label;
+    export let icon;
+    export let href;
+    export let disabled = false;
+</script>
+
+<style>
+.disabled {
+    opacity: 0.5;
+}
+.disabled button {
+    cursor: default;
+}
+.disabled button:focus {
+    outline: none;
+}
+</style>
+
+<a href={disabled ? null : href}
+    class="max-w-sm mx-auto my-2 w-full" class:disabled
+>
+    <button
+        class="bg-blue-990 text-white py-4 text-left rounded-2xl font-semibold flex items-center w-full h-18"
+    >
+        {#if icon}
+            <div class="w-24 flex justify-center">
+                <img alt="{label}-logo" src={icon} />
+            </div>
+            {label}
+        {:else}
+            <p class="justify-center flex w-full">{label}</p>
+        {/if}
+    </button>
+</a>
